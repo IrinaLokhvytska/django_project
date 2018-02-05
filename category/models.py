bf091f1a6b822a1e8953722d3a1331035c8a8973
IrinaLokhvytska <irinaramzaeva2@gmail.com>
2018-02-05 13:59:53 +0200

    category

--- category/models.py
+++ category/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
-    image = models.ImageField(upload_to = 'category/', default = 'category/default.png')
+    image = models.ImageField(upload_to = 'static/category', default = 'static/category/default.png')
 
     def __str__(self):
         return self.name
